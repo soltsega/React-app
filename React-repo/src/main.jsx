6f6769aa7362add8a31368c67e9e64134d90457e
Solomon Tsega <tsegasolomon538@gmail.com>
2025-09-30 13:34:19 +0300

    modified main.jsx

--- React-repo/src/main.jsx
+++ React-repo/src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function Intro() {
+  return (
+    <div className="App">
+      <h1>Hello, React!</h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Intro />
   </StrictMode>,
+
 )
